772c1124392f709214534997cc686650aac30a1a
donacesar <donacesar@gmail.com>
2022-07-23 00:53:29 +0300

    add handler for create post form

---
+++ create_post.php
@@ -0,0 +1,29 @@
+<?php
+
+// берем расширение загруженного файла
+$extension = pathinfo($_FILES['image_file']['name'])['extension'];
+
+// делаем новое уникальное имя для файла
+$name = uniqid() . "." . $extension;
+
+// берем путь временного файла
+$temp_filename = $_FILES['image_file']['tmp_name'];
+
+// сохраняем файл в папку images
+move_uploaded_file($temp_filename, 'images/' . $name);
+
+/* Сохраняем пост в базу */
+
+// подготавливаем поля для базы
+$title = $_POST['title'];
+$image = 'images/' . $name;
+$text = $_POST['text'];
+$date = time();
+
+// добавлем в базу
+$pdo = new PDO("mysql:host=localhost; dbname=bigsnatch", "mysql", "mysql");
+$statement = $pdo->prepare("INSERT INTO posts (title, image, text, date) VALUES (:title, :image, :text, :date)");
+$statement->execute(['title' => $title, 'image' => $image, 'text' => $text, 'date' => $date]);
+
+// редирект на admin.php
+header("Location: admin.php");
--- edit_form.php
+++ edit_form.php
@@ -14,17 +14,17 @@
     <form action="" method="post" enctype="multipart/form-data">
         <div class="form-outline">
             <label class="form-label" for="typeText">+ post title...</label>
-            <input type="text" id="typeText" class="form-control" name="postTitle"/>                        
+            <input type="text" id="typeText" class="form-control" name="title"/>
           </div>
         <br>
         <div class="form-outline">
         <label class="form-label" for="customFile">+ image</label>
-        <input type="file" class="form-control" id="customFile" name="postImg"/>
+        <input type="file" class="form-control" id="customFile" name="image_file"/>
     </div>
         <br>
         <div class="form-outline">
             <label class="form-label" for="textAreaExample">+post content...</label>
-            <textarea class="form-control" id="textAreaExample" rows="4" name="postContent"></textarea>
+            <textarea class="form-control" id="textAreaExample" rows="4" name="text"></textarea>
         </div>
         <br>
         <input type="submit" class="btn btn-success" value="release post">
